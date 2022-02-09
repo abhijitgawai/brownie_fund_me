052f4147c088b76f9bc8e538fb6d03bb3bf1360d
abhijitgawai <abhijit.gawai18@vit.edu>
2022-02-09 12:50:15 +0530

    raising expection for error we are expecting

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,7 +1,7 @@
 from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from scripts.deploy import deploy_fund_me
-# from brownie import network, accounts, exceptions
-# import pytest
+from brownie import network, accounts, exceptions
+import pytest
 
 
 def test_can_fund_and_withdraw():
@@ -16,10 +16,10 @@ def test_can_fund_and_withdraw():
     assert fund_me.addressToAmountFunded(account.address) == 0
 
 
-# def test_only_owner_can_withdraw():
-#     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-#         pytest.skip("only for local testing")
-#     fund_me = deploy_fund_me()
-#     bad_actor = accounts.add()
-#     with pytest.raises(exceptions.VirtualMachineError):
-#         fund_me.withdraw({"from": bad_actor})
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()                                                  # this bad_actor account is not owner, thus will not pass onlyOwner modifier, and thus will raise and error 
+    with pytest.raises(exceptions.VirtualMachineError):                         # But we are expecting this error, so we are reaising expection on this error
+        fund_me.withdraw({"from": bad_actor})
