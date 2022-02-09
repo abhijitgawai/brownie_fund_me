fab97173476c104c1ed9f9e1c95ad0d3e6d32dd2
abhijitgawai <abhijit.gawai18@vit.edu>
2022-02-09 09:56:02 +0530

    in deploy.py fetching 'verify' from brownie-config.yaml

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import FundMe, network, config
+from brownie import FundMe, network, config, MockV3Aggregator
 from scripts.helpful_scripts import get_account
 
 
@@ -6,7 +6,13 @@ def deploy_fund_me():
     account = get_account()
     # if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
     if network.show_active() != "development":
-        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]         # Here network.show_active() is rinkeby
+    else:
+        print(f"The active network is {network.show_active()}")
+        print("Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000000, {"from":account})
+        price_feed_address = mock_aggregator.address
+        print("Mocks Deployed")
     # else:
     #     deploy_mocks()
     #     price_feed_address = MockV3Aggregator[-1].address
@@ -16,7 +22,7 @@ def deploy_fund_me():
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from": account},
-        publish_source=True
+        publish_source=config["networks"][network.show_active()].get("verify")              # instead of []["verify"], using get to avoid index errors 
     )
     print(f"Contract deployed to {fund_me.address}")
     # return fund_me
