07a63e17b08be93f75254e65eaac3029ee3f0a87
abhijitgawai <abhijit.gawai18@vit.edu>
2022-02-09 10:15:26 +0530

    beautified previous commit

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,17 @@
 from brownie import FundMe, network, config, MockV3Aggregator
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import deploy_mocks, get_account
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def deploy_fund_me():
@@ -8,14 +20,9 @@ def deploy_fund_me():
     if network.show_active() != "development":
         price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]         # Here network.show_active() is rinkeby
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying Mocks...")
-        mock_aggregator = MockV3Aggregator.deploy(18, 2000000000000000000000, {"from":account})
-        price_feed_address = mock_aggregator.address
-        print("Mocks Deployed")
-    # else:
-    #     deploy_mocks()
-    #     price_feed_address = MockV3Aggregator[-1].address
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address                                     # Getting lattest MockV3Aggregator address
+
 
     # if we are on a persistant network(rinkeby), use the associated address(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e)
     # otherwise, deploy mocks (fake priceFeed)
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,7 +1,21 @@
-from brownie import network, config, accounts
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import Web3
+
+
+DECIMALS=18
+STARTING_PRICE = 2000                                                       # in deploy_mocks() function used for constructor values of MockV3Aggregator
 
 def get_account():
     if network.show_active() == "development":                              # Devlopment enviormnet will use ganache accounts
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])                  # For real networks we will use our real wallet (test wallet)
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(DECIMALS, Web3.toWei(STARTING_PRICE,"ether"), {"from":get_account()})           # passing constructor values , from account to MockV3Aggregator.sol  //toWei adds 18 decimals
+    price_feed_address = MockV3Aggregator[-1].address                                     # Getting lattest MockV3Aggregator address
+    print("Mocks Deployed")
